e94c3ebf9ecf963712084a67f210ce40b1f42268
dubey-raj <dubey.rajfgp@gmail.com>
2025-02-20 21:39:09 +0530

    added serilog for logging and global exception handler

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -2,9 +2,14 @@ using IdentityService.Configurations;
 using IdentityService.Model;
 using IdentityService.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+using Serilog.Formatting.Json;
+using Serilog;
+using IdentityService.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigureLogging(builder);
 builder.Services.AddControllers();
 builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
@@ -21,9 +26,22 @@ app.UseCors(policy => policy
 .AllowAnyHeader()
 .AllowAnyOrigin()
 );
+app.UseSerilogRequestLogging();
+app.UseMiddleware<GlobalExceptionHandler>();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureLogging(WebApplicationBuilder builder)
+{
+    //Configure serilog as logger
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console(new JsonFormatter(renderMessage: true))
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+    builder.Host.UseSerilog();
+}
