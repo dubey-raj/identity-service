be614d0bcf459bbfa08a94bd596d4dde92638157
dubey-raj <dubey.rajfgp@gmail.com>
2025-03-02 22:15:44 +0530

    ignore healhz call log in serilog

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -6,10 +6,21 @@ using Microsoft.OpenApi.Models;
 using Serilog.Formatting.Json;
 using Serilog;
 using IdentityService.Middlewares;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureLogging(builder);
+builder.Services.AddLogging(logConfig =>
+{
+    logConfig.ClearProviders();
+    logConfig.AddDebug();
+    logConfig.AddEventSourceLogger();
+    ConfigureLogging(builder);
+    logConfig.AddSerilog();
+});
 builder.Services.AddControllers();
 builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
@@ -39,9 +50,23 @@ static void ConfigureLogging(WebApplicationBuilder builder)
 {
     //Configure serilog as logger
     Log.Logger = new LoggerConfiguration()
-        .WriteTo.Console(new JsonFormatter(renderMessage: true))
-        .Enrich.FromLogContext()
+        .WriteTo.Console(new JsonFormatter(renderMessage: false))
         .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder().WithDefaultDestructurers())
+        .Enrich.With(new ExceptionEnricher())
         .CreateLogger();
     builder.Host.UseSerilog();
+}
+
+class ExceptionEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        if (logEvent.Exception == null)
+            return;
+
+        var logEventProperty = propertyFactory.CreateProperty("EscapedException", logEvent.Exception.ToString().Replace("\r\n", "\\r\\n"));
+        logEvent.AddPropertyIfAbsent(logEventProperty);
+    }
 }
